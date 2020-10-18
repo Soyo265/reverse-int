1f78cc805d5a617311a25837cdfc78eb0e689c6b
Soyo265 <soyokaze265@gmail.com>
2020-10-18 13:36:12 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+    return parseInt(number
+        .toString()
+        .split('')
+        .reverse()
+        .join(''));
 }
